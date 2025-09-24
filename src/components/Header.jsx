e799e93462774d712f2e62b653aea97e63a8898e
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-09-24 22:32:19 +0530

    Used the Link from next for navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
@@ -6,9 +7,15 @@ const Header = () => {
       <div className="h-15 flex justify-between items-center bg-gray-200 w-full px-12 py-10 text-black">
         <h1 className="text-2xl font-bold">New App</h1>
         <ul className="flex gap-12">
-          <li>Home</li>
-          <li>About</li>
-          <li>Settings</li>
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/settings"}>Settings</Link>
+          </li>
         </ul>
       </div>
     </header>
